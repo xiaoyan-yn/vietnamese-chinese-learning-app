0568af16abf2bf0356e6a81290347a267e0426fc
xiaoyan-yn <539843669@qq.com>
2026-06-21 15:57:45 +0800

    Fix Vercel deployment: eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,5 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
   {
@@ -20,7 +12,8 @@ const eslintConfig = [
       ".npm-cache/**",
     ],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
 ];
 
 export default eslintConfig;
